e22e666c2930e62c650f108362cb023f2e00ad8e
sujal mallick <mallicksujal626@gmail.com>
2026-01-09 11:37:50 +0530

    feat: initialize backend for Invest-Hub project
    - Add package.json with dependencies and scripts
    - Create readme.md for project description
    - Set up Express app with CORS and cookie parser
    - Define database name constant
    - Implement MongoDB connection logic
    - Create entry point for server with environment variable support

---
+++ backend/readme.md
@@ -0,0 +1,1 @@
+Backend for Invest-Hub project
---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import connectDB from './db/conn.js';
+import { app } from './app.js';
+
+
+dotenv.config({
+    path: './.env'
+});
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        }
+        )
+    })
+    .catch((err) => {
+        console.log(`DB connection error:`, err);
+    })
